c8e46cdae5fe7c775f48958b242297efcf3e5a55
Ernest Igwe <i.ernest@pulse.digital>
2021-01-05 21:19:25 +0100

    testing object clones

--- public/main.php
+++ public/main.php
@@ -1,61 +1,48 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 require 'autoload.php';
+
 use FunwithelePHPant\Datastructures\LinkedList\{LinkedList, StringNode};
 use FunwithelePHPant\Config\Config;
 
 //$stack = new LinkedList();
 //stream_set_blocking(STDIN, 0);
 
-class Node {
-     public $val = null;
-     public $prev = null;
-     public $next = null;
-     public $child = null;
-     function __construct($val = 0) {
+class Node
+{
+    public $val = null;
+    public $next = null;
+    public $random = null;
+
+    function __construct($val = 0)
+    {
         $this->val = $val;
-        $this->prev = null;
         $this->next = null;
-        $this->child = null;
-     }
- }
-
- $a = new Node(1);
- $b = new Node(2);
- $c = new Node(6);
- $d = new Node(7);
- $e = new Node(3);
-
- $a->next = $b;
- $b->prev = $a;
- $b->child = $c;
- $b->next = $e;
- $e->prev = $b;
- $b->child->next = $d;
- $d->prev = $b->child;
-
-function flatten($head) {
-   $flathead = recFlatten($head);
-   return $flathead;
-}
-
-function recFlatten($flatHead) {
-    if (!$flatHead) return $flatHead;
-    $down = recFlatten($flatHead->child);
-    $right = recFlatten($flatHead->next);
-    if ($down !== null) {
-        $curr = $down;
-        while ($curr->next !== null) $curr = $curr->next;
-
-        if ($right !== null) {
-            $curr->next = $right;
-            $right->prev = $curr;
-        }
-        $flatHead->next = $down;
-        $down->prev = $flatHead;
-        $flatHead->child = null;
+        $this->random = null;
     }
-    return $flatHead;
 }
 
-var_dump(flatten($a));
+$a = new Node(7);
+$b = new Node(13);
+$c = new Node(11);
+$d = new Node(10);
+$e = new Node(1);
+
+$a->next = $b;
+
+$b->next = $c;
+$b->random = $a;
+
+$c->next = $d;
+$c->random = $e;
+
+$d->next = $e;
+$d->random = $c;
+
+$e->random = $a;
+
+function copyRandomList($head) {
+
+}
+
+var_dump($a, clone($a));
